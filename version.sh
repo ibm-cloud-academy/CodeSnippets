65ab6b8c23183f8b9a8351b496a25a1eea5b00a1
palistra <palistra@us.ibm.com>
2017-04-10 10:56:32 -0400

    Added docker, cf, bx

--- version.sh
+++ version.sh
@@ -8,23 +8,45 @@ hline () {
 clear
 hline =
 
+## Ubuntu version
 lsb_release -a
 hline
 
+## Git version
 git --version
 hline
 
+## Git version
 java -version
 hline
 
+## Websphere Application Server version
 /opt/software/wlp/bin/server version
 hline
 
+## node version
 echo -n "node version "
 node -v
 hline
 
+## npm version
 echo -n "npm version "
 npm -v
+hline
+
+## Docker version
+docker version
+hline
+
+## Cloud Foundry version
+echo -n "Cloud Foundry version "
+cf version
+hline
+
+## Bluemix version
+echo -n "Cloud Foundry version "
+bx -version
+hline
+
 hline =
 echo
